cd9282f4717325dc6836bd78df5fedd6ddd8f21a
jasonclark <jasclark@gmail.com>
2015-03-26 00:02:24 -0600

    removed extra <head> tag

--- code.php
+++ code.php
@@ -13,7 +13,6 @@ $customScript[0] = 'none';
 <meta charset="utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta name="viewport" content="width=device-width,initial-scale=1"/>
-<head>
 <title><?php echo($pageTitle); ?> - jason a. clark</title>
 <link rel="alternate" type="application/rss+xml" title="diginit - jason clark" href="http://feeds.feedburner.com/diginit" />
 <?php if ($customCSS != 'none') {
